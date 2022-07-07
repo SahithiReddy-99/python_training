780045c38102a4d391f1594fcd351515f46e6a9a
Sahithi Reddy Aendapally <52377852+SahithiReddy-99@users.noreply.github.com>
2022-07-07 12:25:03 +0530

    Update sahithi_student_height.py

--- sahithi_student_height.py
+++ sahithi_student_height.py
@@ -1,37 +1,34 @@
 class Student:
-    def __init__(self,name,heigth,weigth,age):
+    def __init__(self,name,height,weigth,age):
         self.name=name
-        self.heigth=heigth
+        self.height=height
         self.weigth=weigth
         self.age=age
     def __str__(self) :
-        return "name = "+self.name+" "+ "heigth in cm= "+str(self.heigth)
+        return "name = "+self.name+" "+ "height in cm= "+str(self.height)
     def __lt__(self,other):
-        return self.heigth<other.heigth
+        return self.height<other.height
     def __gt__(self,other):
-        return self.heigth<other.heigth
-
-
+        return self.height<other.height
 
 record=[]
 
-istrue=True
-while(istrue):
-    print(" enter 1 add a student \n two view heigth of a student \n 3  change the heigth of a student \n 4  view entire student list")
+while(True):
+    print(" enter 1 add a student \n two view height of a student \n 3  change the height of a student \n 4  view entire student list")
     print (" 5 to exit")
     x=input()
     if x=='1':
-        print("enter the name,heigth,weigth and age in seperate lines")
+        print("enter the name,height,weigth and age in seperate lines")
         name=input()
-        heigth=float(input())
+        height=float(input())
         weigth=float(input())
         age=int(input())
-        student=Student(name,heigth,weigth,age)
+        student=Student(name,height,weigth,age)
         record.append(student)
         print(student, end=" ")
         print(" is added to list")
     if (x=='2'):
-        print(" enter name of student to view heigth")
+        print(" enter name of student to view height")
         isinrecord=False
         name=input()
         for ele in record:
@@ -41,20 +38,20 @@ while(istrue):
         if(isinrecord==False):
             print("there is no such student with name"+str(name))
     if (x=='3'):
-        print(" enter name of student and  heigth of student to change heigth")
+        print(" enter name of student and  height of student to change height")
         name=input()
-        heigth=float(input())
+        height=float(input())
         isinrecord=False
 
         for ele in sorted(record):
             if(ele.name==name):
                 isinrecord=True
-                print("the heigth of student is changed from "+str(ele.heigth)+" to "+str(heigth))
-                ele.heigth=heigth
+                print("the height of student is changed from "+str(ele.height)+" to "+str(height))
+                ele.height=height
         if(isinrecord==False):
             print("there is no such student with name"+str(name))
     if(x=='4'):
         for ele in record:
             print(ele)
     if(x=='5'):
-        isture=False
+        break
